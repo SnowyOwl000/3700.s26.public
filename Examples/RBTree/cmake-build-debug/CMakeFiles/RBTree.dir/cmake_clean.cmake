c6822a4099444b58020a867e68a225b648868aed
Robert Kramer <rwkramer@ysu.edu>
2026-04-17 13:37:44 -0400

    red-black tree code

---
+++ Examples/RBTree/cmake-build-debug/CMakeFiles/RBTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RBTree.dir/link.d"
+  "CMakeFiles/RBTree.dir/main.cpp.o"
+  "CMakeFiles/RBTree.dir/main.cpp.o.d"
+  "RBTree"
+  "RBTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RBTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
